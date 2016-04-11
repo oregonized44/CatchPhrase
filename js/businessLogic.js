39ef7b79a028757522c7519b96b0ef890bddb023
Connor Cummings and John Klein <john@>
2016-04-11 13:48:32 -0700

    laid out the ground work for functionality on our pages

---
+++ js/businessLogic.js
@@ -0,0 +1,54 @@
+
+
+
+
+function Team(name,playerNumbers){    // contstructs our team object
+this.teamName = name;
+this.players = playerNumbers;
+this.score = 0;
+this.nextPlayer = 1;
+}
+function Game(){       // constructs our game object
+}
+
+Team.prototype.won = function(){
+  this.score++;
+
+
+}
+
+Team.prototype. = function () {
+
+};
+function play(){     // play button on click willlllll
+  set team name value to our name val in our constructor
+  sets amount of players in our constructor
+  loads following page
+}
+
+function pass(){      //what happens when we click pass
+pops the current word in the display
+pushes prior word out
+change team
+}
+function nextRound(){
+  update round number
+  reset timer
+  update score
+  update active player
+}
+function over(){
+  is the numer of players.length array equal to the number of rounds?
+  figure out the total score
+  alert winning team
+  display next page
+}
+function reset(){
+ reloads page or sends you back to the main page
+
+}
+function replay(){
+  send back to the second page
+
+
+}
