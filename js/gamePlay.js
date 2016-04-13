9ee94663b2a6d26fe40d435488388bb64439869f
John <johnmbklein@gmail.com>
2016-04-13 09:30:51 -0700

    updated round-over UI; added active team and active player display; updated timer appearance

--- js/gamePlay.js
+++ js/gamePlay.js
@@ -1,10 +1,25 @@
+function updateActiveTeamAndPlayerDisplay () {
+  if (game.activeTeam === 1) {
+    $('#active-team').text(team1.teamName);
+  } else if (game.activeTeam === 2) {
+    $('#active-team').text(team2.teamName);
+  }
+  if (game.activeTeam === 1) {
+    $('#active-player').text(team1.activePlayer);
+  } else if (game.activePlayer === 2) {
+    $('#active-player').text(team2.activePlayer);
+  }
+};
+
 gamePlayOnLoad();
+updateActiveTeamAndPlayerDisplay();
 $('#phrase-display').text(newWord());
 $('#round-num').text(game.roundNumber);
-$('#timer-display').text("2:00")
+$('#timer-display').text("2:00");
 $('#next-word').click(function(){
   nextWordButtonOnClick();
   $('#phrase-display').text(newWord());
+  updateActiveTeamAndPlayerDisplay();
 });
 
 function getTimeRemaining(endtime) {
@@ -28,8 +43,6 @@ function initializeClock(id, endtime) {
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
     if (t.total <= 0) {
       clearInterval(timeinterval);
-      var audio = new Audio('scream.mp3');
-      audio.play();
       timerExpires();
 
     }
@@ -39,5 +52,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 5 * 1000);
+var deadline = new Date(Date.parse(new Date()) + 60 * 1000);
 initializeClock('clockdiv', deadline);
