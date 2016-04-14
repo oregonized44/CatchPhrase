9079dac6f3679b8da3156e7c845cee86b139561c
John <johnmbklein@gmail.com>
2016-04-14 15:14:02 -0700

    basic leaderboard functionality: iterates through learboard object. If teamName matches existing key, increments wins; if teamName does not match existing key, adds teamName to leaderboard.

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,7 +1,19 @@
 function Leaderboard() {
-  teamname: 2;
+  this.cats = 2;
 };
 
-Object.prototype.addToLeaderboard(team) {
+leaderboard = new Leaderboard;
 
+Object.prototype.addToLeaderboard = function () {
+  var winningTeamName = this.teamName.toLowerCase();
+  var inLeaderBoard = false;
+  for (var key in leaderboard) {
+    if (winningTeamName === key) {
+      leaderboard[key]++;
+      inLeaderBoard = true;
+    }
+    if (!inLeaderBoard) {
+      leaderboard[winningTeamName] = 1;
+    }
+  }
 };
