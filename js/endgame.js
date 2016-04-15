19e708596163747d85da8c0d387abe159af96681
LaTaevia <lataevia.berry@gmail.com>
2016-04-15 12:08:55 -0700

    something

--- js/endgame.js
+++ js/endgame.js
@@ -1,3 +1,9 @@
+//Function to calculate current score as a percentage of total possible points
+function calculateScorePercentage(team){
+  var scorePercentage = (team.score / game.maxRounds) * 100;
+  return 'width: ' + scorePercentage + '%';
+};
+
 endGameOnLoad();
 if (team1.score > team2.score) {
   $('#winning-team').text(team1.teamName);
@@ -12,3 +18,10 @@ $('#replay-game').click(function(){
 $('#reset-game').click(function(){
   resetButtonClick();
 });
+
+$('#team1-progress-bar').attr('style', calculateScorePercentage(team1));
+$('#team2-progress-bar').attr('style', calculateScorePercentage(team2));
+$('#team1-score').text(team1.score);
+$('#team2-score').text(team2.score);
+$('#team1-name').text(team1.teamName);
+$('#team2-name').text(team2.teamName);
